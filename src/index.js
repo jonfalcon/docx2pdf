36987861f96c79ffec62003534a1b8bd801de477
Luiz Carlos <nonam4@yahoo.com>
2022-11-03 17:20:14 -0300

    Return the converted pdf and the HTML too

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-const mammoth = require('mammoth');
-const chromium = require('chrome-aws-lambda');
+const mammoth = require( 'mammoth' )
+const chromium = require( 'chrome-aws-lambda' )
 
 /**
- * pdf2docx
+ * docx2pdfhtml
  * 
- * Converts docx to pdf.
+ * Converts docx to pdf or html.
  * 
  * @param  fpIn   {string}  Path to the pdf file
+ * @returns {pdf: Readable, html: string}
  */
-async function pdf2docx (fpIn) {
-  let browser;
+async function pdf2docx ( fpIn ) {
+  let browser
 
   try {
-    const html = (await mammoth.convertToHtml({ path: fpIn })).value;
-    browser = await chromium.puppeteer.launch({
+    const html = ( await mammoth.convertToHtml( {path: fpIn} ) ).value
+    browser = await chromium.puppeteer.launch( {
       args: chromium.args,
       defaultViewPort: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: true,
       ignoreHTTPSErrors: true
-    });
-    
-    const page = await browser.newPage();
-    await page.emulateMediaType('screen');
-    await page.setContent(html, { waitUntil: 'networkidle0' });
+    } )
 
-    const pdfStream = await page.createPDFStream({
+    const page = await browser.newPage()
+    await page.emulateMediaType( 'screen' )
+    await page.setContent( html, {waitUntil: 'networkidle0'} )
+
+    const pdf = await page.createPDFStream( {
       format: 'A4',
       margin: {
         top: '50px',
@@ -35,17 +36,17 @@ async function pdf2docx (fpIn) {
       },
       preferCSSPageSize: true,
       printBackground: true,
-    });
+    } )
 
-    pdfStream.on('end', async () => {
-      await browser.close();
-    });
+    pdf.on( 'end', async () => {
+      await browser.close()
+    } )
 
-    return pdfStream;
-  } catch (err) {
-    browser && await browser.close();
-    throw err;
+    return {pdf, html}
+  } catch ( err ) {
+    browser && await browser.close()
+    throw err
   }
 }
 
-module.exports = pdf2docx;
+module.exports = pdf2docx
